157272de330801d823d74983afd20b368711abe5
Egor Zinovichev <ezinovichev@gmail.com>
2023-03-07 11:14:54 +0300

    Update test.erl

--- src/test.erl
+++ src/test.erl
@@ -3,9 +3,9 @@
 -include_lib("eunit/include/eunit.hrl").
 
 simple_test() ->
-  Response = net_kernel:start(['discovery@127.0.0.1', longnames]),
+  Response = net_kernel:start(['discovery@lab', longnames]),
 
-  spawn('discovery@127.0.0.1', fun() ->
+  spawn('discovery@lab', fun() ->
       discovery_server:start(),
       Egor = mpeer:join("Egor"),
       Andrew = mpeer:join("Andrew"),
