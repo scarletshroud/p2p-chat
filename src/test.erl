709431ac2348c7c60f5781abe26ec6eb4bf16877
Egor Zinovichev <ezinovichev@gmail.com>
2023-03-07 05:56:12 +0300

    Update test.erl

--- src/test.erl
+++ src/test.erl
@@ -3,23 +3,21 @@
 -include_lib("eunit/include/eunit.hrl").
 
 simple_test() ->
-
   Response = net_kernel:start(['discovery@127.0.0.1', longnames]),
-  net_kernel:start(['client@127.0.0.1', longnames]),
-  net_kernel:start(['client2', shortnames]),
-
-  io:format("~w", [node()]),
 
   spawn('discovery@127.0.0.1', fun() ->
-    discovery_server:start()
-    end),
-
-  spawn('client@127.0.0.1', fun() ->
-    mpeer:join("e")
-    end),
-
-  spawn('client2', fun() ->
-    mpeer:join("a")
+      discovery_server:start(),
+      Egor = mpeer:join("Egor"),
+      Andrew = mpeer:join("Andrew"),
+      Klim = mpeer:join("Klim"),
+      Evgeniy = mpeer:join("Evgeniy"),
+      client:send_message(Egor, "Privet"),
+      timer:sleep(1000),
+      client:send_message(Andrew, "Hello"),
+      timer:sleep(1000),
+      client:send_message(Klim, "I'm from London"),
+      timer:sleep(1000),
+      client:send_message(Evgeniy, "I'm from Russia")
     end),
 
   Response.
