520828635b9bcff93d699f9fd31d853159e19943
Egor Zinovichev <ezinovichev@gmail.com>
2023-03-07 14:24:05 +0300

    Update demo_app.erl

--- src/demo_app.erl
+++ src/demo_app.erl
@@ -5,6 +5,18 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-  test:simple_test().
+  Response = discovery_server:start(),
+  Egor = mpeer:join("Egor"),
+  Andrew = mpeer:join("Andrew"),
+  Klim = mpeer:join("Klim"),
+  Evgeniy = mpeer:join("Evgeniy"),
+  client:send_message(Egor, "Privet"),
+  timer:sleep(1000),
+  client:send_message(Andrew, "Hello"),
+  timer:sleep(1000),
+  client:send_message(Klim, "I'm from London"),
+  timer:sleep(1000),
+  client:send_message(Evgeniy, "I'm from Russia"),
+  Response.
 
 stop(_State) -> ok.
